88a787ce3342beeb1d96ecee4a44fec229863795
ageofrage <agehara.dev@gmail.com>
2023-04-11 00:04:00 +0900

    collect()

--- src/fizz_buzz.rs
+++ src/fizz_buzz.rs
@@ -1,9 +1,14 @@
 pub fn calculation() {
-    let fz = |x: i32| match (x % 3, x % 5) {
-        (0, 0) => format!("FizzBuzz"),
-        (0, _) => format!("Fizz"),
-        (_, 0) => format!("Buzz"),
-        _ => x.to_string(),
-    };
-    (1..=100).map(fz).for_each(|s| println!("{}", s));
+    fn fz(x: i32) -> String {
+        match (x % 3, x % 5) {
+            (0, 0) => format!("FizzBuzz"),
+            (0, _) => format!("Fizz"),
+            (_, 0) => format!("Buzz"),
+            _ => x.to_string(),
+        }
+    }
+
+    let res = (1..=100).map(fz).collect::<Vec<_>>().join("!\n");
+
+    println!("{}", res);
 }
