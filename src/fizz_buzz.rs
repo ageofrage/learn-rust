c92554d61fda53b65dc4e49260a9619572520258
ageofrage <agehara.dev@gmail.com>
2023-04-30 12:18:55 +0900

    adhoc

--- src/fizz_buzz.rs
+++ src/fizz_buzz.rs
@@ -15,9 +15,12 @@ pub fn calculation() {
         fn to_str(&self, x: T) -> String;
     }
 
-    impl ToFzStr<i64> for FizzBuzz<i64> {
-        fn to_str(&self, x: i64) -> String {
-            match (x % self.div_a == self.zero, x % self.div_b == self.zero) {
+    fn common_fizz_buzz<T>(x: T, div_a: T, div_b: T, zero: T) -> String
+    where
+        T: std::ops::Rem<T, Output = T> + Eq + Copy + ToString,
+    {
+        {
+            match (x % div_a == zero, x % div_b == zero) {
                 (true, true) => format!("FizzBuzz"),
                 (true, _) => format!("Fizz"),
                 (_, true) => format!("Buzz"),
@@ -26,7 +29,19 @@ pub fn calculation() {
         }
     }
 
+    impl ToFzStr<u32> for FizzBuzz<u32> {
+        fn to_str(&self, x: u32) -> String {
+            common_fizz_buzz(x, self.div_a, self.div_b, self.zero)
+        }
+    }
+
+    impl ToFzStr<i64> for FizzBuzz<i64> {
+        fn to_str(&self, x: i64) -> String {
+            common_fizz_buzz(x, self.div_a, self.div_b, self.zero)
+        }
+    }
+
     (1..=100)
-        .map(|x| FizzBuzz::new(3, 5, 0).to_str(x))
+        .map(|x: u32| FizzBuzz::new(3, 5, 0).to_str(x))
         .for_each(|x| println!("{}", x))
 }
